13568b32d846bdc5bfe15d1baa75748f29d2530c
Jaakko Vanhala <jaakko.vanhala@kamk.fi>
2024-03-13 20:36:11 +0000

    Update file duck.py

--- code/duck.py
+++ code/duck.py
@@ -1,8 +1,20 @@
 """ Tässä kelpo esimerkki DuckDB:n suorituskyvystä vs. any-other-database
 """
+import time
 import duckdb
 
-data = duckdb.sql("SELECT COUNT(*) FROM './data/projekti1/*.csv' WHERE timestamp BETWEEN '2019-04-01 00:00:00' AND '2019-04-31 04:25:00'" )
-#data = duckdb.sql("SELECT COUNT(*) FROM './data/projekti1/*.csv'" )
+start = time.time()
+
+data = duckdb.sql("SELECT COUNT(*) FROM './data/projekti1/*.csv' WHERE timestamp \
+                   BETWEEN '2019-04-01 00:00:00' AND '2019-04-31 00:00:00'" )
 
 print(data)
+
+mid = time.time()
+
+data.to_csv("tulos.csv")
+
+end = time.time()
+
+print(f"Read/Parse: {mid-start}")
+print(f"CSV-output: {end-mid}")
