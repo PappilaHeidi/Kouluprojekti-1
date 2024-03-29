ddc5c7ef62c483d7fef8f47c09fb1b6870812a31
Heidi <heidipap@kamk.fi>
2024-03-29 11:12:23 +0200

    lisätty streamlit

---
+++ code/streamlit/streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Projektin streamlit", # nimi voidaan vaihtaa myöhemmin
+    page_icon="💻",
+    layout = "wide"
+)
+
+st.markdown("""
+            # Tämä on esimerkki. Täydennetään lisää, kun projekti etenee! 😁""")
+
