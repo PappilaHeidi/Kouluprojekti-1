3d969b7bd7ce638e227c66dad8be9c38ce493c67
Joni <jonikauppinen@kamk.fi>
2024-04-22 18:52:47 +0300

    Nimimuutokset ja mainsivu.pt roskiin

--- code/streamlit/pages/gigakoodi.py
+++ code/streamlit/pages/gigakoodi.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout = "wide"
 )
 
-st.title('Andreaksen GAkoodi')
+st.title('Andreaksen GIGAkoodi')
 
 st.markdown("""
             * Valinta minkä kärryn dataa halutaan kuvata (Dropdown)
@@ -26,68 +26,4 @@ st.markdown("""
 
 
 
-# Read data from DuckDB
-def read_node(tbl, node):
-    conn = dd.connect(database='Silver_SensorData')
-    query = "SELECT * FROM Silver_SensorData WHERE node_id = 3200"
-    df = conn.execute(query).fetch_df()
-    return df
-
-# Function to draw data on the image
-def draw_on_image(df):
-    img_path = '../code/kauppa.jpg/'
-    img = Image.open(img_path)
-    draw = ImageDraw.Draw(img)
-    
-    # Calibration parameters
-    x_offset = 111
-    y_offset = 23
-    x_scale = 1166/10500
-    y_scale = 563/5150
-    
-    def scale_coords(x, y):
-        xr = (x * x_scale) + x_offset
-        yr = (y * y_scale) + y_offset
-        return xr, yr
-
-    for _, row in df.iterrows():
-        x, y = scale_coords(row['x'], row['y'])
-        draw.rectangle((x, y, x + 2, y + 2), fill='red')
-    
-    return img
-
-# Streamlit app
-st.title('Drawing Testfield Data')
-
-# Read data
-tbl = "Silver_SensorData"
-node = "3200"
-df = read_node(tbl, node)
-
-# Display data
-st.subheader('Data')
-st.write(df)
-
-# Filter and process data
-df_lim = df[(df['x'] >= 550)]
-df_lim['time_diff'] = df_lim['timestamp'].diff()
-
-iloc_list = []
-for index, value in df_lim.iterrows():
-    if value['time_diff'] > pd.Timedelta(minutes=5):
-        iloc_list.append(index)
-
-paths_values = np.arange(0, len(iloc_list), 1)
-for i in range(len(iloc_list) - 1):
-    start_index = iloc_list[i]
-    end_index = iloc_list[i + 1]
-    df_lim.loc[start_index:end_index, "paths"] = paths_values[i]
-
-# Draw data on the image
-st.subheader('Draw Data on Image')
-img = draw_on_image(df_lim)
-st.image(img, caption='Data on Image', use_column_width=True)
-
-# Display number of paths
-st.subheader('Number of Paths')
-st.write(len(iloc_list) - 1)
+# Poistin koodin täältä sillä se ei toiminut + Andreas on tehnyt GIGAcharts
